ec0d8272e3c35218b76354c18f7bb1b497357331
mohanediiur777-spec <mohanediiur777@gmail.com>
2025-10-23 20:25:33 +0300

    Update APPS_SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 
 const CONFIG = {
     // ⚠️ REPLACE THIS with your actual Apps Script deployment URL
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyt1FBbG7i5r6-MWFCUh_TKHK8PEIB6RYii6f1NHLnw4oXQzJZSaCnmmhDPPm6tZLSn/exec",
+    APPS_SCRIPT_URL: "APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbztKMM9RqAfM4K6qbVtTulAehOHptESmiy_gVdM01gtBycOqsVvArM_bxb1Kd_-AP7K/exec",",
     
     // Logo URLs (already configured)
     LOGO_EAND: "https://ik.imagekit.io/xtj3m9hth/image-remove1bg-preview%20(3).png?updatedAt=1761220721716",
