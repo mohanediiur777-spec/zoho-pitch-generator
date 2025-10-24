ce02607650168f2c788d1d22929b1a2d1ba7dd87
mohanediiur777-spec <mohanediiur777@gmail.com>
2025-10-24 06:05:59 +0300

    Update APPS_SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Configuration File
 const CONFIG = {
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbztKMM9RqAfM4K6qbVtTulAehOHptESmiy_gVdM01gtBycOqsVvArM_bxb1Kd_-AP7K/exec",
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyRF0HV855inyb0YTv5GxpC9l0I8cMGzS5172qPr_70LcZcriMAoBIh9vzMjavcfv8y/exec",
     
     // Logo URLs
     LOGO_EAND: "https://ik.imagekit.io/xtj3m9hth/image-remove1bg-preview%20(3).png?updatedAt=1761220721716",
