f32fe43d6ab8e3451b33c42b6cf654850ed19c23
mohanediiur777-spec <mohanediiur777@gmail.com>
2025-10-25 06:56:05 +0300

    Add configuration constants to config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzJE6RT8NZ3sNq0ZGn4ed-_mQfbgJh0z4EpmPNJIa7mGWcg_Yh6Uf2ZLc4hA6yRDA9P/exec',
+  LOGO_EAND: 'https://ik.imagekit.io/xtj3m9hth/image-remove1bg-preview%20(3).png?updatedAt=1761220721716',
+  LOGO_ZOHO: 'https://ik.imagekit.io/xtj3m9hth/image-removebg-preview%20(3).png?updatedAt=1761220721361',
+  DEFAULT_LANGUAGE: 'en',
+  ENABLE_PRESENTATION_MODE: true,
+  ENABLE_FEATURE_SUGGESTIONS: true
+};
