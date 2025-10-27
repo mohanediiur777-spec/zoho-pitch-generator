88baf855b6258554cd24060aa4dea86c8711fa20
mohanediiur777-spec <mohanediiur777@gmail.com>
2025-10-27 08:54:38 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzJE6RT8NZ3sNq0ZGn4ed-_mQfbgJh0z4EpmPNJIa7mGWcg_Yh6Uf2ZLc4hA6yRDA9P/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw4-AYJHnFtWZFGLK7LynEntAK4Kt-ksLjlAZSPHW90Sg0kdoDCR9OH2YkgqTqmAdAF/exec',
   LOGO_EAND: 'https://ik.imagekit.io/xtj3m9hth/image-remove1bg-preview%20(3).png?updatedAt=1761220721716',
   LOGO_ZOHO: 'https://ik.imagekit.io/xtj3m9hth/image-removebg-preview%20(3).png?updatedAt=1761220721361',
   DEFAULT_LANGUAGE: 'en',
